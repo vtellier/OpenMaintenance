6afa9202021a8228336bd47d14717f5360d72173
Vincent Tellier <hello@vincenttellier.com>
2026-05-07 12:14:22 -0400

    feat: complete migration from Fiber to Echo

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,6 @@ module github.com/vtellier/OpenMaintenance
 go 1.25.0
 
 require (
-	github.com/getkin/kin-openapi v0.137.0
 	github.com/gofiber/fiber/v2 v2.52.13
 	github.com/labstack/echo/v4 v4.15.2
 	github.com/mattn/go-sqlite3 v1.14.44
@@ -13,30 +12,20 @@ require (
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
-	github.com/go-openapi/jsonpointer v0.21.0 // indirect
-	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/labstack/gommon v0.5.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
-	github.com/oasdiff/yaml v0.0.9 // indirect
-	github.com/oasdiff/yaml3 v0.0.12 // indirect
-	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/santhosh-tekuri/jsonschema/v6 v6.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	github.com/woodsbury/decimal128 v1.3.0 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
 	golang.org/x/text v0.36.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/time v0.15.0 // indirect
 )
--- backend/internal/routes/intervention.go
+++ backend/internal/routes/intervention.go
@@ -2,72 +2,72 @@ package routes
 
 import (
 	"database/sql"
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 	dbpackage "github.com/vtellier/OpenMaintenance/internal/db"
 	"github.com/vtellier/OpenMaintenance/internal/models"
 	"strconv"
 )
 
-func SetupInterventionRoutes(api fiber.Router, database *sql.DB) {
-	api.Get("/interventions", func(c *fiber.Ctx) error {
+func SetupInterventionRoutes(api *echo.Group, database *sql.DB) {
+	api.GET("/interventions", func(c echo.Context) error {
 		interventions, err := dbpackage.ListInterventions(database)
 		if err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			return c.JSON(500, map[string]string{"error": err.Error()})
 		}
-		return c.JSON(interventions)
+		return c.JSON(200, interventions)
 	})
 
-	api.Get("/tasks/:task_id/interventions", func(c *fiber.Ctx) error {
-		taskID, _ := strconv.Atoi(c.Params("task_id"))
+	api.GET("/tasks/:task_id/interventions", func(c echo.Context) error {
+		taskID, _ := strconv.Atoi(c.Param("task_id"))
 		interventions, err := dbpackage.ListInterventionsByTask(database, taskID)
 		if err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			return c.JSON(500, map[string]string{"error": err.Error()})
 		}
-		return c.JSON(interventions)
+		return c.JSON(200, interventions)
 	})
 
-	api.Post("/interventions", func(c *fiber.Ctx) error {
+	api.POST("/interventions", func(c echo.Context) error {
 		intervention := new(models.Intervention)
-		if err := c.BodyParser(intervention); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+		if err := c.Bind(intervention); err != nil {
+			return c.JSON(400, map[string]string{"error": err.Error()})
 		}
 
 		if err := dbpackage.CreateIntervention(database, intervention); err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			return c.JSON(500, map[string]string{"error": err.Error()})
 		}
 
-		return c.JSON(intervention)
+		return c.JSON(201, intervention)
 	})
 
-	api.Get("/interventions/:id", func(c *fiber.Ctx) error {
-		id, _ := strconv.Atoi(c.Params("id"))
+	api.GET("/interventions/:id", func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
 		intervention, err := dbpackage.GetIntervention(database, id)
 		if err != nil {
-			return c.Status(404).JSON(fiber.Map{"error": "Intervention not found"})
+			return c.JSON(404, map[string]string{"error": "Intervention not found"})
 		}
-		return c.JSON(intervention)
+		return c.JSON(200, intervention)
 	})
 
-	api.Put("/interventions/:id", func(c *fiber.Ctx) error {
-		id, _ := strconv.Atoi(c.Params("id"))
+	api.PUT("/interventions/:id", func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
 		intervention := new(models.Intervention)
-		if err := c.BodyParser(intervention); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+		if err := c.Bind(intervention); err != nil {
+			return c.JSON(400, map[string]string{"error": err.Error()})
 		}
 		intervention.ID = id
 
 		if err := dbpackage.UpdateIntervention(database, intervention); err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			return c.JSON(500, map[string]string{"error": err.Error()})
 		}
 
-		return c.JSON(intervention)
+		return c.JSON(200, intervention)
 	})
 
-	api.Delete("/interventions/:id", func(c *fiber.Ctx) error {
-		id, _ := strconv.Atoi(c.Params("id"))
+	api.DELETE("/interventions/:id", func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
 		if err := dbpackage.DeleteIntervention(database, id); err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			return c.JSON(500, map[string]string{"error": err.Error()})
 		}
-		return c.SendStatus(204)
+		return c.NoContent(204)
 	})
 }
